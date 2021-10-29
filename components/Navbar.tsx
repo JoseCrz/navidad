ff99f6f85a3c71519566a32ae1385a01f3c8204a
Jose Cuevas <josecueram@gmail.com>
2021-10-29 12:35:24 -0500

    Create basic Navbar Component

---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Image from "next/image";
+import { Flex, Button } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+
+import { Menu } from "./Menu";
+
+export function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <Flex
+        bg="brown"
+        height="56px"
+        justifyContent="space-between"
+        alignItems="center"
+        px={4}
+      >
+        <Image
+          src="/juguetron-logo.svg"
+          alt="Juguetron logo"
+          width="126.602px"
+          height="24.618"
+        />
+        <Button bg="transparent" padding={0}>
+          <HamburgerIcon
+            color="white"
+            width="32px"
+            height="32px"
+            onClick={() => setIsOpen(true)}
+          />
+        </Button>
+      </Flex>
+      {isOpen && <Menu onRequestClose={() => setIsOpen(false)} />}
+    </>
+  );
+}
