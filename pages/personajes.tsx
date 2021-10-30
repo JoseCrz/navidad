4992d02e43ad21c1bc5c66e9c404a1a290dc6bbf
Jose Cuevas <josecueram@gmail.com>
2021-10-29 20:09:54 -0500

    Add initial implementation of personajes logic

--- pages/personajes.tsx
+++ pages/personajes.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import Head from "next/head";
-import Image from "next/image";
-import { Box, Flex, Circle, Text } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { Navbar } from "components/Navbar";
-import { Story } from "components/Story";
+import { ProfileSelector } from "components/ProfileSelector";
+import { StoryViewer } from "components/StoryViewer";
+import { profiles } from "data/profiles";
 
 export default function Personajes() {
+  const [selectedProfileIndex, setSelecterProfileIndex] = useState(0);
+
+  const selectedProfile = profiles[selectedProfileIndex];
+
   return (
     <>
       <Head>
@@ -16,58 +22,21 @@ export default function Personajes() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <main>
-        <Box height="100vh" pt="56px">
-          <Flex
-            bg="white"
-            width="100%"
-            minHeight="116px"
-            justifyContent="center"
-            boxShadow="0 3px 6px 0 rgba(0, 0, 0, 0.16)"
-            zIndex="1"
-            position="relative"
-          >
-            <Flex alignItems="center">
-              <Box textAlign="center" mx={2}>
-                <Circle border="2px solid gray" overflow="hidden">
-                  <Image
-                    src="/esferin-profile.jpg"
-                    alt="Imagen de Perfil de Esferin"
-                    width="66px"
-                    height="66px"
-                  />
-                </Circle>
-                <Text>Esferin</Text>
-              </Box>
-              <Box textAlign="center" mx={2}>
-                <Circle border="2px solid gray" overflow="hidden">
-                  <Image
-                    src="/esferin-profile.jpg"
-                    alt="Imagen de Perfil de Esferin"
-                    width="66px"
-                    height="66px"
-                  />
-                </Circle>
-                <Text>Esferin</Text>
-              </Box>
-              <Box textAlign="center" mx={2}>
-                <Circle border="2px solid gray" overflow="hidden">
-                  <Image
-                    src="/esferin-profile.jpg"
-                    alt="Imagen de Perfil de Esferin"
-                    width="66px"
-                    height="66px"
-                  />
-                </Circle>
-                <Text>Esferin</Text>
-              </Box>
-            </Flex>
-          </Flex>
-          <Box height="calc(100vh - 172px)">
-            <Story />
-          </Box>
+      <Box as="main" height="100vh" pt="56px">
+        <ProfileSelector
+          profiles={profiles}
+          selectedIndex={selectedProfileIndex}
+          onSelectProfile={(selectedIndex) =>
+            setSelecterProfileIndex(selectedIndex)
+          }
+        />
+        <Box height="calc(100vh - 172px)">
+          <StoryViewer
+            key={selectedProfile.name}
+            stories={selectedProfile.stories}
+          />
         </Box>
-      </main>
+      </Box>
     </>
   );
 }
