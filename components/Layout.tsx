e6169d2b5790a07da8f00f47cddb1c2b5781cf3f
Jose Cuevas <josecueram@gmail.com>
2021-11-01 13:31:09 -0600

    Create Layout Component

---
+++ components/Layout.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import { Box } from "@chakra-ui/react";
+import type { BoxProps } from "@chakra-ui/react";
+import { Navbar } from "./Navbar";
+
+export function Layout(props: BoxProps) {
+  return (
+    <>
+      <Head>
+        <title>El Secreto de la Navidad</title>
+        <meta
+          name="description"
+          content="Juguetron: El Secreto de la Navidad"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <Box as="main" {...props} />
+    </>
+  );
+}
