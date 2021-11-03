152f37d59f9507ab27a4514e1d7add258deb188d
Jose Cuevas <josecueram@gmail.com>
2021-11-03 13:12:10 -0600

    Update Section component API

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,12 +1,12 @@
-import { Container } from "@chakra-ui/react";
-import type { ContainerProps } from "@chakra-ui/react";
+import { Box, Container } from "@chakra-ui/react";
+import type { BoxProps } from "@chakra-ui/react";
 
-export function Section({ children, ...rest }: ContainerProps) {
+export function Section({ children, ...props }: BoxProps) {
   return (
-    <section>
-      <Container px={8} py={12} {...rest}>
+    <Box as="section" {...props}>
+      <Container px={8} py={12}>
         {children}
       </Container>
-    </section>
+    </Box>
   );
 }
