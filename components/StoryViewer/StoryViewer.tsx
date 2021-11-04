4f68a6d12f0441530808b7228b6d9cf8fa054bb7
Jose Cuevas <josecueram@gmail.com>
2021-11-04 10:28:08 -0600

    Restructure StoryViewer

--- components/StoryViewer/StoryViewer.tsx
+++ components/StoryViewer/StoryViewer.tsx
@@ -5,42 +5,46 @@ import { times } from "utils";
 import type { Story } from "types";
 
 type StoryViewerProps = {
-  stories: Story[];
+  story: Story;
+  totalStories: number;
+  onStoryCompleted: () => void;
   onStoriesCompleted: () => void;
 };
 
-export function StoryViewer({ stories, onStoriesCompleted }: StoryViewerProps) {
-  const [currentStoryIndex, setCurrentStoryIndex] = useState(0);
-
-  const story = stories[currentStoryIndex];
-  const [storyProgress, setStoryProgress] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setStoryProgress((prev) => prev + 1);
-    }, 150); // change time interval as needed -> 15000 / seconds total you wish each story to last
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    if (storyProgress === 100) {
-      if (currentStoryIndex + 1 == stories.length) {
-        onStoriesCompleted();
-      } else {
-        setCurrentStoryIndex((prevIndex) => {
-          if (prevIndex === stories.length - 1) return 0;
-
-          return prevIndex + 1;
-        });
-
-        setStoryProgress(0);
-      }
-    }
-  }, [storyProgress, stories.length, currentStoryIndex, onStoriesCompleted]);
-
+export function StoryViewer({
+  story,
+  totalStories,
+  onStoryCompleted,
+  onStoriesCompleted,
+}: StoryViewerProps) {
   return (
     <Box width="100%" height="100%" position="relative">
+      <StoryView
+        story={story}
+        totalStories={totalStories}
+        onStoryCompleted={onStoryCompleted}
+        onStoriesCompleted={onStoriesCompleted}
+      />
+      <StoryControls />
+    </Box>
+  );
+}
+
+type StoryViewProps = {
+  story: Story;
+  totalStories: number;
+  onStoryCompleted: () => void;
+  onStoriesCompleted: () => void;
+};
+
+function StoryView({
+  story,
+  totalStories,
+  onStoryCompleted,
+  onStoriesCompleted,
+}: StoryViewProps) {
+  return (
+    <>
       <Image
         priority
         key={story.title}
@@ -51,44 +55,90 @@ export function StoryViewer({ stories, onStoriesCompleted }: StoryViewerProps) {
         placeholder="blur"
       />
       <StoryOverlay>
-        <StoryHeader>
-          <StorySteps
-            totalSteps={stories.length}
-            currentStoryProgress={storyProgress}
-            currentStoryIndex={currentStoryIndex}
-          />
-          <Heading
-            as="h3"
-            size="md"
-            color="whiteAlpha.800"
-            letterSpacing="widest"
-          >
-            {story.title}
-          </Heading>
-        </StoryHeader>
-        <StoryDescription description={story.description} />
+        <Flex flexDirection="column" justifyContent="space-between">
+          <StoryHeader>
+            <StorySteps
+              totalStories={totalStories}
+              onStoryCompleted={onStoryCompleted}
+              onStoriesCompleted={onStoriesCompleted}
+            />
+            <Heading
+              as="h3"
+              size="md"
+              color="whiteAlpha.800"
+              letterSpacing="widest"
+            >
+              {story.title}
+            </Heading>
+          </StoryHeader>
+          <StoryDescription description={story.description} />
+        </Flex>
       </StoryOverlay>
-    </Box>
+    </>
+  );
+}
+
+function StoryControls() {
+  return (
+    <Flex position="absolute" width="100%" height="100%">
+      <Box height="100%" width="50%" bg="#ff000038" />
+      <Box height="100%" width="50%" bg="#0000ff30" />
+    </Flex>
   );
 }
 
 type StoryStepsProps = {
-  totalSteps: number;
-  currentStoryProgress: number;
-  currentStoryIndex: number;
+  totalStories: number;
+  onStoryCompleted: () => void;
+  onStoriesCompleted: () => void;
 };
 
 function StorySteps({
-  totalSteps,
-  currentStoryProgress,
-  currentStoryIndex,
+  totalStories,
+  onStoryCompleted,
+  onStoriesCompleted,
 }: StoryStepsProps) {
+  const [currentStoryIndex, setCurrentStoryIndex] = useState(0);
+
+  const [storyProgress, setStoryProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setStoryProgress((prev) => prev + 1);
+    }, 36); // change time interval as needed -> 15000 / seconds total you wish each story to last
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (storyProgress === 100) {
+      if (currentStoryIndex + 1 === totalStories) {
+        onStoriesCompleted();
+      } else {
+        onStoryCompleted();
+        setStoryProgress(0);
+        setCurrentStoryIndex((prevIndex) => {
+          if (prevIndex === totalStories - 1) return 0;
+
+          return prevIndex + 1;
+        });
+      }
+    }
+  }, [
+    onStoryCompleted,
+    storyProgress,
+    setCurrentStoryIndex,
+    totalStories,
+    currentStoryIndex,
+    onStoriesCompleted,
+  ]);
+
   return (
-    <Grid templateColumns={`repeat(${totalSteps}, 1fr)`} gap={3} my={6}>
-      {times(totalSteps, (index: number) => {
+    <Grid templateColumns={`repeat(${totalStories}, 1fr)`} gap={3} my={6}>
+      {times(totalStories, (index: number) => {
         const progress =
           index === currentStoryIndex
-            ? currentStoryProgress
+            ? storyProgress
             : index < currentStoryIndex
             ? 100
             : 0;
@@ -123,15 +173,7 @@ type StoryOverlayProps = {
 
 function StoryOverlay({ children }: StoryOverlayProps) {
   return (
-    <Flex
-      flexDirection="column"
-      justifyContent="space-between"
-      position="absolute"
-      top="0"
-      right="0"
-      left="0"
-      bottom="0"
-    >
+    <Flex position="absolute" top="0" right="0" left="0" bottom="0">
       {children}
     </Flex>
   );
--- pages/personajes.tsx
+++ pages/personajes.tsx
@@ -8,9 +8,20 @@ import { StoryViewer } from "components/StoryViewer";
 import { profiles } from "data/profiles";
 
 export default function Personajes() {
-  const [selectedProfileIndex, setSelecterProfileIndex] = useState(0);
+  const [selectedProfileIndex, setSelectedProfileIndex] = useState(0);
+  const [currentStoryIndex, setCurrentStoryIndex] = useState(0);
 
   const selectedProfile = profiles[selectedProfileIndex];
+  const currentStory = selectedProfile.stories[currentStoryIndex];
+
+  function nextStory() {
+    setCurrentStoryIndex((prev) => prev + 1);
+  }
+
+  function nextProfile() {
+    setCurrentStoryIndex(0);
+    setSelectedProfileIndex((prev) => (prev + 1) % profiles.length);
+  }
 
   return (
     <Layout height="100vh" bg="blackAlpha.800">
@@ -19,19 +30,16 @@ export default function Personajes() {
           profiles={profiles}
           selectedIndex={selectedProfileIndex}
           onSelectProfile={(selectedIndex) =>
-            setSelecterProfileIndex(selectedIndex)
+            setSelectedProfileIndex(selectedIndex)
           }
         />
         <Box height="calc(100vh - 172px)">
           <StoryViewer
             key={selectedProfile.name}
-            stories={selectedProfile.stories}
-            onStoriesCompleted={() =>
-              setSelecterProfileIndex((prev) => {
-                if (prev + 1 === profiles.length) return 0;
-                return prev + 1;
-              })
-            }
+            story={currentStory}
+            totalStories={selectedProfile.stories.length}
+            onStoryCompleted={nextStory}
+            onStoriesCompleted={nextProfile}
           />
         </Box>
       </Box>
