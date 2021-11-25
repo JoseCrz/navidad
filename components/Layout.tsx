e4ef62385e4f739b5e9e1a900e19b9ab934b3411
Jose Cuevas <josecueram@gmail.com>
2021-11-25 16:07:11 -0600

    Add Google Analytics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 import { Box } from "@chakra-ui/react";
 import type { BoxProps } from "@chakra-ui/react";
 import { Navbar } from "./Navbar";
@@ -14,6 +15,25 @@ export function Layout(props: BoxProps) {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5NQ3DQF');`,
+        }}
+      />
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5NQ3DQF"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
       <Navbar />
       <Box as="main" pt={14} {...props} />
     </>
