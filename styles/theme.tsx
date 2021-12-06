b482266caf223247786c4b05aed44364d1321e91
Jose Cuevas <josecueram@gmail.com>
2021-12-05 20:38:31 -0600

    Add colors to theme

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -12,11 +12,13 @@ const theme = extendTheme({
       greenLight: "#4d9c05",
       teal: "#1e6859",
       tealLighter: "#f0f8f6",
+      cyan: "#58bcb9",
       text: {
         white: "#fcfcfc",
         red: "#720a0a",
         yellow: "#e1b63b",
         green: "#083f24",
+        blue: "#154667",
       },
       bg: {
         yellow: "#e2b73b",
