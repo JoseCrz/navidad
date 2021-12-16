648e746c2d9706e3586ce8f80306359afee8e6de
Jose Cuevas <josecueram@gmail.com>
2021-12-15 22:39:13 -0600

    Create index for Components

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+export { Layout } from "./Layout";
+export { LinkStyled } from "./LinkStyled";
+export { Loader } from "./Loader";
+export { Menu } from "./Menu";
+export { Navbar } from "./Navbar";
+export { ProfileSelector } from "./ProfileSelector";
+export { Section } from "./Section";
