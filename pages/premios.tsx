19d0d131098d2429702cd86d7e049c2741dfcadb
Jose Cuevas <josecueram@gmail.com>
2021-12-15 23:31:10 -0600

    Add empty Premios Page

---
+++ pages/premios.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "components";
+import { Heading } from "@chakra-ui/react";
+
+export default function Premios() {
+  return (
+    <Layout>
+      <Heading>Premios</Heading>
+    </Layout>
+  );
+}
