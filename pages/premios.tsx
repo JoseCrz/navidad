d4a43dce5c851bf43b8e74e9f6110296edcc3237
CarlosRuiz <ingecarlosrm@gmail.com>
2021-12-20 14:24:07 -0600

    [UPDATE] images premios

--- pages/premios.tsx
+++ pages/premios.tsx
@@ -1,10 +1,21 @@
 import { Layout } from "components";
-import { Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Text,
+  Heading,
+  Grid,
+  GridItem,
+  Link,
+} from "@chakra-ui/react";
 
 export default function Premios() {
   return (
     <Layout>
-      <Heading>Premios</Heading>
+      <Box bg='brand.green' w='100%' p={4} color='white'>
+      <Heading>Más de $50,000 mxn en premios</Heading>
+        This is the Box
+      </Box>
     </Layout>
   );
 }
